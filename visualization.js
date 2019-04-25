2b088cb1c641b605e8cab1a7c60cdf5cab27d688
jatinm2 <jatinm2@illinois.edu>
2019-04-25 00:09:01 -0500

    started some preliminary analysis and got it showing on the map as basic red circles

--- visualization.js
+++ visualization.js
@@ -43,4 +43,46 @@ var displayBaseMap = function (mapData) {
 
     console.log("done rendering base map!");
 
+    // monitors slider
+    var data = null;
+    d3.csv('dataViz/summary_cleaned.csv', function(error, d) {
+        data = d;
+        const year = document.getElementById('yearSlider').value;
+        visualize(data, year, mapLayer, projection);
+    });
+
+    document.getElementById('yearSlider').oninput = function() {
+        mapLayer.selectAll('circle').remove();
+        const year = document.getElementById('yearSlider').value;
+        visualize(data, year, mapLayer, projection);
+    }
+}
+
+const visualize = function (data, year, mapLayer, projection) {
+
+    const globalMax = 660;
+    const globalMin = 0;
+
+    var radiusScale = d3.scale.linear()
+                        .domain([globalMin, globalMax])
+                        .range([2, 30]);
+
+    mapLayer.selectAll('circle')
+            .data(data).enter()
+            .append('circle')
+            .attr('cx', function (d) {
+                const toProject = [d['Longitude'], d['Latitude']];
+                return projection(toProject)[0];
+            })
+            .attr('cy', function (d) {
+                const toProject = [d['Longitude'], d['Latitude']];
+                return projection(toProject)[1];
+            })
+            .attr('r', function (d) {
+                return radiusScale(d[year]);
+            })
+            .attr('fill', function (d) {
+                return d3.hsl(0, 1, 0.5);
+            });
+
 }
